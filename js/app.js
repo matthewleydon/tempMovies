e90e36136634fbd1eaae85c64277df5c97305764
Matthew Leydon <lazdich@hotmail.co.uk>
2022-02-19 16:48:50 +0000

    New stuff

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ fetch('https://imdb-api.com/en/API/Top250Movies/k_6gcebc9a')
 // =======================================================================================
 // local copy of the data
 funcLocalData = (movies) => {
+  let movieData = []
   movies.map((m) => {
     movieData.push(
       {
@@ -22,13 +23,18 @@ funcLocalData = (movies) => {
         comments: ''
       });
   });
-  console.log(movieData)
+  // console.log(movieData)
+  localStorage.setItem('MovieDataStored', JSON.stringify(movieData))
 };
+
+
 // =======================================================================================
 
 // =======================================================================================
 // create cards
 funcCreateCards = (newMovieData) => {
+  let tempMovieData = JSON.parse(localStorage.getItem('MovieDataStored'))
+  console.log(tempMovieData)
   let tempCardData = '';
   newMovieData.map((nMD) => {
     tempCardData += `<div class="movieCard">
@@ -40,10 +46,11 @@ funcCreateCards = (newMovieData) => {
     <h2>${nMD.title}</h2>
     <p>${nMD.year}</p>
     <p>IMDB Rating: <span>${nMD.imDbRating}</span></p>
+    ${nMD.comments ? `<p><span style="font-weight: bold">Comment: </span>${nMD.comments}</p>` : ''}
     <div class="movieCardSocial">
       <a href="#" onClick="funcLike('${nMD.id}')"><i class="fas fa-heart ${nMD.likes ? 'likeHeart' : ''}"></i></a>
       <a href="https://www.imdb.com/title/${nMD.id}/" target="_blank"><i class="fas fa-share-alt"></i></a>
-      <a href="#"><i class="fas fa-comment"></i></a>
+      <a href="#" onClick="funcComment('${nMD.id}')"><i class="fas fa-comment ${nMD.comments ? 'commented' : ''}"></i></a>
     </div>
   </div>
 </div>`
@@ -55,6 +62,8 @@ document.querySelector('body').innerHTML = tempCardData;
 // =======================================================================================
 // Links Clicked
 funcLike = (i) => {
+
+let movieData = JSON.parse(localStorage.getItem('MovieDataStored'));
 
   movieData = movieData.map((m) => {
     if (m.id === i) {
@@ -71,3 +80,24 @@ funcLike = (i) => {
 };
 
 // =======================================================================================
+// Submit a Comment
+funcComment = (i) => {
+  // console.log('comment', i);
+
+  let tempComment = prompt('Please submit your comment')
+
+  // console.log(tempComment)
+
+  movieData = movieData.map((m) => {
+    if (m.id === i) {
+      m.comments = tempComment;
+    }
+    return m;
+  });
+
+  // movieData.sort(function (a, b) {
+  //   return b.likes - a.likes;
+  // });
+
+  funcCreateCards(movieData);
+};
